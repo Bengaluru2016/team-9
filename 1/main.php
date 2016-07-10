77f5a8204647e49a8a6ce6f12dd9132766f74c87
MaanviBandi <maanvi.bandi15@gmail.com>
2016-07-10 09:37:03 +0530

    1final

---
+++ 1/main.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href='https://fonts.googleapis.com/css?family=Playfair+Display:700italic,700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="project-cards-template/css/reset.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="project-cards-template/css/style.css"> <!-- Resource style -->
+	<script src="project-cards-template/js/modernizr.js"></script> <!-- Modernizr -->
+ 
+</head>
+<body>
+	<header>
+		
+	</header>
+
+	<nav class="cd-primary-nav">
+		<ul>
+			<li class="cd-label">Logout</li>
+		</ul>
+	</nav> <!-- .cd-primary-nav -->
+	
+	<div class="cd-projects-container">
+		<ul>
+			<li class="single-project">
+				<a href="create.php"><div class="cd-title">
+					<h2>Create Content</h2>
+                </div>
+				</a>
+			</li>
+
+			<li class="single-project">
+				<div class="cd-title">
+					<a href="manage.php"><br><br><br><br><h2>Manage Content</h2>
+				</div> <!-- .cd-title -->
+            </li>
+            <li class="single-project">
+				<div class="cd-title">
+				 <a href="users.php"><br><br><br><br>
+					<h2>Assess Users</h2>
+				</div> <!-- .cd-title -->
+            </li>
+		</ul>
+	</div> <!-- .cd-projects-container -->
+<script src="js/jquery-2.1.4.js"></script>
+<script src="js/main.js"></script> <!-- Resource jQuery -->
+</body>
+</html>
