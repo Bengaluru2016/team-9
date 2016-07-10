dbea29165333ab89a54c1c683587d636024d9021
Prateek Varma <prateek2k@gmail.com>
2016-07-10 07:56:45 +0530

    Deployment Prelim

---
+++ final_last/nav/nav2/challenge.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="css/challenge/reset.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="css/challenge/style.css"> <!-- Resource style -->
+	<script src="js/challenge/modernizr.js"></script> <!-- Modernizr -->
+  	
+	<title>Challenge</title>
+</head>
+<body>
+	<header>
+		<h1>Challenge</h1>
+        <center> <font size="20" >658 points so far!</font></center>
+        <br>
+	</header>
+
+	<section class="cd-section">
+		<p>
+           <br>
+          <img src="img/challenge.png"/>
+            <br>
+             <h1>Kudos!! :D</h1>
+		</p>
+
+		
+       
+        <div class="cd-modal-action">
+			<a href="./redeem.php" class="btn" data-type="modal-trigger">Redeem your points</a>
+			<span class="cd-modal-bg"></span>
+		</div> <!-- cd-modal-action -->
+<br>
+    <div class="cd-modal-action">
+			<a href="#0" class="btn" data-type="modal-trigger">Challenge your friends</a>
+			<span class="cd-modal-bg"></span>
+		</div> <!-- cd-modal-action -->
+        <br>
+    <a href="http://facebook.com" target="_blank"><img src="./img/share.png"/></a>
+		<div class="cd-modal">
+			<div class="cd-modal-content">
+				<h2>
+		<fieldset>		
+           
+<form action="demo_form.asp" method="get">
+  <input type="checkbox" name="vehicle" value="Bike"> Kshamitha<br>
+  <input type="checkbox" name="vehicle" value="Car" > Maanvi<br>  <input type="checkbox" name="vehicle" value="Bike">Ajay<br>
+  <input type="checkbox" name="vehicle" value="Car"> Shreya<br>  <input type="checkbox" name="vehicle" value="Bike">Vijay<br>
+  <input type="checkbox" name="vehicle" value="Car" >Prateek<br>  <input type="checkbox" name="vehicle" value="Bike">Sanjay<br>
+  <input type="checkbox" name="vehicle" value="Car" > Sindura<br>
+  <input type="submit" value="Submit">
+                </form>
+                    </fieldset>
+				</h2>
+			</div> <!-- cd-modal-content -->
+		</div> <!-- cd-modal -->
+
+		<a href="#0" class="cd-modal-close">Close</a>
+	</section> <!-- .cd-section -->
+	
+<script src="js/challenge/jquery-2.1.1.js"></script>
+<script src="js/challenge/velocity.min.js"></script>
+<script src="js/challenge/main.js"></script> <!-- Resource jQuery -->
+</body>
+</html>
---
+++ final_last/nav/nav2/myprogress.php
@@ -0,0 +1,349 @@
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href='https://fonts.googleapis.com/css?family=Vollkorn|Lato:400,700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="css/myprogress/reset.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="css/myprogress/style.css"> <!-- Resource style -->
+    
+    <!--modalview-->
+    <link rel="stylesheet" href="css/modal/reset.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="css/modal/style.css"> <!-- Resource style -->
+	<script src="js/modal/modernizr.js"></script> <!-- Modernizr -->
+  	
+    
+    <!--timeline-->
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link href='https://fonts.googleapis.com/css?family=Playfair+Display:700,900|Fira+Sans:400,400italic' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="css/timeline/reset.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="css/timeline/style.css"> <!-- Resource style -->
+	<script src="js/timeline/modernizr.js"></script> <!-- Modernizr -->
+    
+    <!-- nav-->
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="css/nav/reset.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="css/nav/style.css"> <!-- Resource style -->
+	<script src="js/modernizr.js"></script> <!-- Modernizr -->
+    
+	<title>VillGrow Learn</title>
+</head>
+<body>
+  
+  
+      <!--modalview-->
+    <main class="cd-main-content">
+		<section class="center">
+			<h1>Your progress</h1>
+            <!--timeline-->
+            <section class="cd-horizontal-timeline">
+	<div class="timeline">
+		<div class="events-wrapper">
+			<div class="events">
+				<ol>
+					<li><a href="#0" data-date="16/01/2014" class="selected">16 Jan</a></li>
+					<li><a href="#0" data-date="28/02/2014">28 Feb</a></li>
+					<li><a href="#0" data-date="20/04/2014">20 Mar</a></li>
+					<li><a href="#0" data-date="20/05/2014">20 May</a></li>
+					<li><a href="#0" data-date="09/07/2014">09 Jul</a></li>
+					<li><a href="#0" data-date="30/08/2014">30 Aug</a></li>
+					<li><a href="#0" data-date="15/09/2014">15 Sep</a></li>
+					<li><a href="#0" data-date="01/11/2014">01 Nov</a></li>
+					<li><a href="#0" data-date="10/12/2014">10 Dec</a></li>
+					<li><a href="#0" data-date="19/01/2015">29 Jan</a></li>
+					<li><a href="#0" data-date="03/03/2015">3 Mar</a></li>
+				</ol>
+
+				<span class="filling-line" aria-hidden="true"></span>
+			</div> <!-- .events -->
+		</div> <!-- .events-wrapper -->
+			
+		<ul class="cd-timeline-navigation">
+			<li><a href="#0" class="prev inactive">Prev</a></li>
+			<li><a href="#0" class="next">Next</a></li>
+		</ul> <!-- .cd-timeline-navigation -->
+	</div> <!-- .timeline -->
+
+	<div class="events-content">
+		<ol>
+			<li class="selected" data-date="16/01/2014">
+				<h2>Progress Timeline</h2>
+				<em>January 16th, 2014</em>
+				<p>	
+					Completed module: Organization with basics.
+				</p>
+			</li>
+
+			<li data-date="28/02/2014">
+				<h2>Earned medal: Basic trainer.</h2>
+				<em>February 28th, 2014</em>
+				
+			</li>
+
+			<li data-date="20/04/2014">
+				<h2>Module stated: Managing with time.</h2>
+				<em>March 20th, 2014</em>
+				
+			</li>
+
+			<li data-date="20/05/2014">
+				<h2>Completed module 3. Hurray!</h2>
+				<em>May 20th, 2014</em>
+				
+			</li>
+
+			<li data-date="09/07/2014">
+				<h2>You completed Ravi's challenge</h2>
+				<em>July 9th, 2014</em>
+				
+			</li>
+
+			<li data-date="30/08/2014">
+				<h2>You earned badge: Major! </h2>
+				<em>August 30th, 2014</em>
+			
+			</li>
+
+            
+			<li data-date="15/09/2014">
+				<h2>You completed Anita's challenge</h2>
+				<em>September 15th, 2014</em>
+			
+			</li>
+
+			<li data-date="01/11/2014">
+				<h2>Module 3 started.</h2>
+				<em>November 1st, 2014</em>
+			
+			</li>
+
+			<li data-date="10/12/2014">
+				<h2>Module 3 completed!</h2>
+				<em>December 10th, 2014</em>
+			
+			</li>
+
+			<li data-date="19/01/2015">
+				<h2>Challenged completed!</h2>
+				<em>January 19th, 2015</em>
+			
+			</li>
+
+			<li data-date="03/03/2015">
+				<h2>Event title here</h2>
+				<em>March 3rd, 2015</em>
+			
+			</li> 
+          
+		</ol>
+	</div> <!-- .events-content -->
+</section>
+	
+<script src="js/timeline/jquery-2.1.4.js"></script>
+<script src="js/timeline/jquery.mobile.custom.min.js"></script>
+<script src="js/timeline/main.js"></script> <!-- Resource jQuery -->
+            
+            <!--timeline-->
+	<div class="cd-cover-layer"></div> <!-- .cd-cover-layer -->
+<script src="js/modal/jquery-2.1.4.js"></script>
+<script src="js/modal/snap.svg-min.js"></script>
+<script src="js/modal/main.js"></script> <!-- Resource jQuery -->
+    <!--modalview ends-->
+	<div class="cd-radial-slider-wrapper">
+		<ul class="cd-radial-slider" data-radius1="60" data-radius2="1364" data-centerx1="110" data-centerx2="1290">
+			<li class="visible">
+				<div class="svg-wrapper">
+					<svg viewBox="0 0 1400 800">
+						<title>Courses</title>
+						<defs>
+							<clipPath id="cd-image-1">
+								<circle id="cd-circle-1" cx="110" cy="400" r="1364"/>
+							</clipPath>
+						</defs>
+
+						<image height='800px' width="1400px" clip-path="url(#cd-image-1)" xlink:href="img/img-1.jpg"></image>
+					</svg>
+				</div> <!-- .svg-wrapper -->
+
+				<div class="cd-radial-slider-content">
+					<div class="wrapper">
+						<div>
+							<h2>Module: Processes within processes</h2>
+							<p>Something about the course.</p>
+							<p>Something about the course.</p>
+							<a href="#0" class="cd-btn">Start Module</a>
+						</div>
+					</div>
+				</div> <!-- .cd-radial-slider-content -->
+			</li>
+
+			<li class="next-slide">
+				<div class="svg-wrapper">
+					<svg viewBox="0 0 1400 800">
+						<title>Courses</title>
+						<defs>
+							<clipPath id="cd-image-2">
+								<circle id="cd-circle-2" cx="1290" cy="400" r="60"/>
+							</clipPath>
+						</defs>
+
+						<image height='800px' width="1400px" clip-path="url(#cd-image-2)" xlink:href="img/img-2.jpg"></image>
+					</svg>
+				</div> <!-- .svg-wrapper -->
+
+				<div class="cd-radial-slider-content">
+					<div class="wrapper">
+						<div>
+							<h2>Module: Go Organised</h2>
+							<p>Something about the course.</p>
+							<p>Something about the course.</p>
+							<a href="#0" class="cd-btn">Start Module</a>
+						</div>
+					</div>
+				</div> <!-- .cd-radial-slider-content -->
+			</li>
+
+			<li>
+				<div class="svg-wrapper">
+					<svg viewBox="0 0 1400 800">
+						<title>Course</title>
+						<defs>
+							<clipPath id="cd-image-3">
+								<circle id="cd-circle-3" cx="110" cy="400" r="60"/>
+							</clipPath>
+						</defs>
+
+						<image height='800px' width="1400px" clip-path="url(#cd-image-3)" xlink:href="img/img-3.jpg"></image>
+					</svg>
+				</div> <!-- .svg-wrapper -->
+
+				<div class="cd-radial-slider-content">
+					<div class="wrapper">
+						<div>
+							<h2>Module: Managing Risks</h2>
+							<p>Something about the course.</p>
+							<p>Something about the course.</p>
+							<a href="#0" class="cd-btn">Start Module</a>
+                           
+                            
+                            
+                            
+                            
+						</div>
+					</div>
+				</div> <!-- .cd-radial-slider-content -->
+			</li>
+
+			<li class="prev-slide">
+				<div class="svg-wrapper">
+					<svg viewBox="0 0 1400 800">
+						<title>Course</title>
+						<defs>
+							<clipPath id="cd-image-4">
+								<circle id="cd-circle-4" cx="110" cy="400" r="60"/>
+							</clipPath>
+						</defs>
+						
+						<image height='800px' width="1400px" clip-path="url(#cd-image-4)" xlink:href="img/img-4.jpg"></image>
+					</svg>
+				</div> <!-- .svg-wrapper -->
+
+				<div class="cd-radial-slider-content">
+					<div class="wrapper">
+						<div>
+							<h2>End of course</h2>
+							<p>Congrats!</p>
+							<p>You've done it soldier!</p>
+                          
+                             <button onclick="myFunction()">  <a href="./challenge.php" class="cd-btn">Finish</a></button>
+
+<p id="demo"></p>
+
+<script>
+function myFunction() {
+    var person = prompt("Please enter std SSI value to mark end of course.");
+    
+    if (person != null) {
+        document.getElementById("demo").innerHTML =
+        "Hello " + person + "! How are you today?";
+    }
+}
+</script>
+                            
+						</div>
+					</div>
+				</div> <!-- .cd-radial-slider-content -->
+			</li>
+		</ul> <!-- .cd-radial-slider -->
+
+		<ul class="cd-radial-slider-navigation">
+			<li><a href="#0" class="next">Next</a></li>
+			<li><a href="#0" class="prev">Prev</a></li>
+		</ul> <!-- .cd-radial-slider-navigation -->
+		
+		<div class="cd-round-mask">
+			<svg viewBox="0 0 1400 800">
+				<defs>
+					<mask id="cd-left-mask" height='800px' width="1400px" x="0" y="0" maskUnits="userSpaceOnUse">
+						<path fill="white" d="M0,0v800h1400V0H0z M110,460c-33.137,0-60-26.863-60-60s26.863-60,60-60s60,26.863,60,60S143.137,460,110,460z"/>
+				    </mask>
+
+				    <mask id="cd-right-mask" height='800px' width="1400px" x="0" y="0" maskUnits="userSpaceOnUse">
+						<path fill="white" d="M0,0v800h1400V0H0z M1290,460c-33.137,0-60-26.863-60-60s26.863-60,60-60s60,26.863,60,60S1323.137,460,1290,460z"/>
+				    </mask>
+				</defs>
+			</svg>
+		</div>
+	</div> <!-- .cd-radial-slider-wrapper -->
+  
+  
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<script>
+	if( !window.jQuery ) document.write('<script src="js/myprogress/jquery-2.2.1.min.js"><\/script>');
+</script>
+<script src="js/myprogress/snap.svg-min.js"></script>
+<script src="js/myprogress/main.js"></script> <!-- Resource jQuery -->
+			<a href="#0" class="cd-btn" id="modal-trigger" data-type="cd-modal-trigger">Continue current course</a>
+		</section>
+	</main> <!-- .cd-main-content -->
+
+	<div class="cd-modal" data-modal="modal-trigger">
+		<div class="cd-svg-bg" 
+		data-step1="M-59.9,540.5l-0.9-1.4c-0.1-0.1,0-0.3,0.1-0.3L864.8-41c0.1-0.1,0.3,0,0.3,0.1l0.9,1.4c0.1,0.1,0,0.3-0.1,0.3L-59.5,540.6 C-59.6,540.7-59.8,540.7-59.9,540.5z" 
+		data-step2="M33.8,690l-188.2-300.3c-0.1-0.1,0-0.3,0.1-0.3l925.4-579.8c0.1-0.1,0.3,0,0.3,0.1L959.6,110c0.1,0.1,0,0.3-0.1,0.3 L34.1,690.1C34,690.2,33.9,690.1,33.8,690z" 
+		data-step3="M-465.1,287.5l-0.9-1.4c-0.1-0.1,0-0.3,0.1-0.3L459.5-294c0.1-0.1,0.3,0,0.3,0.1l0.9,1.4c0.1,0.1,0,0.3-0.1,0.3 l-925.4,579.8C-464.9,287.7-465,287.7-465.1,287.5z" 
+		data-step4="M-329.3,504.3l-272.5-435c-0.1-0.1,0-0.3,0.1-0.3l925.4-579.8c0.1-0.1,0.3,0,0.3,0.1l272.5,435c0.1,0.1,0,0.3-0.1,0.3 l-925.4,579.8C-329,504.5-329.2,504.5-329.3,504.3z" 
+		data-step5="M341.1,797.5l-0.9-1.4c-0.1-0.1,0-0.3,0.1-0.3L1265.8,216c0.1-0.1,0.3,0,0.3,0.1l0.9,1.4c0.1,0.1,0,0.3-0.1,0.3L341.5,797.6 C341.4,797.7,341.2,797.7,341.1,797.5z" 
+		data-step6="M476.4,1013.4L205,580.3c-0.1-0.1,0-0.3,0.1-0.3L1130.5,0.2c0.1-0.1,0.3,0,0.3,0.1l271.4,433.1c0.1,0.1,0,0.3-0.1,0.3 l-925.4,579.8C476.6,1013.6,476.5,1013.5,476.4,1013.4z">
+			<svg height="100%" width="100%" preserveAspectRatio="none" viewBox="0 0 800 500">
+				<title>Model 3: Getting started with organisation</title>
+				<path id="cd-changing-path-1" d="M-59.9,540.5l-0.9-1.4c-0.1-0.1,0-0.3,0.1-0.3L864.8-41c0.1-0.1,0.3,0,0.3,0.1l0.9,1.4c0.1,0.1,0,0.3-0.1,0.3L-59.5,540.6 C-59.6,540.7-59.8,540.7-59.9,540.5z"/>
+				<path id="cd-changing-path-2" d="M-465.1,287.5l-0.9-1.4c-0.1-0.1,0-0.3,0.1-0.3L459.5-294c0.1-0.1,0.3,0,0.3,0.1l0.9,1.4c0.1,0.1,0,0.3-0.1,0.3 l-925.4,579.8C-464.9,287.7-465,287.7-465.1,287.5z"/>
+				<path id="cd-changing-path-3" d="M341.1,797.5l-0.9-1.4c-0.1-0.1,0-0.3,0.1-0.3L1265.8,216c0.1-0.1,0.3,0,0.3,0.1l0.9,1.4c0.1,0.1,0,0.3-0.1,0.3L341.5,797.6 C341.4,797.7,341.2,797.7,341.1,797.5z"/>
+			</svg>
+		</div>
+
+		<div class="cd-modal-content">
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad modi repellendus, optio eveniet eligendi molestiae? Fugiat, temporibus! A rerum pariatur neque laborum earum, illum voluptatibus eum voluptatem fugiat, porro animi tempora? Sit harum nulla, nesciunt molestias, iusto aliquam aperiam est qui possimus reprehenderit ipsam ea aut assumenda inventore iste! Animi quaerat facere repudiandae earum quisquam accusamus tempora, delectus nesciunt, provident quae aliquam, voluptatum beatae quis similique in maiores repellat eligendi voluptas veniam optio illum vero! Eius, dignissimos esse eligendi veniam.
+			</p>
+
+		<iframe width="374" height="180" src="https://www.youtube.com/embed/2XkV6IpV2Y0" frameborder="0" allowfullscreen></iframe>
+            <br><br>
+        <center><a href="./index.php"><button>Go to Home</button></a></center>
+		</div> <!-- cd-modal-content -->
+
+		<a href="#0" class="modal-close">Close</a>
+	</div> <!-- cd-modal -->
+
+</body>
+</html>
